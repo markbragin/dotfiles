1969ea8dd6175e0c3ae27069dcb218b8fb44aab9
mark bragin <m4rk.brag1n@gmail.com>
2023-03-27 23:54:43 +0300

    Last sync: 27-03-2023 23:54:43

--- nvim/lua/user/plugins/lsp/mason.lua
+++ nvim/lua/user/plugins/lsp/mason.lua
@@ -17,11 +17,12 @@ mason.setup()
 mason_lspconfig.setup({
   -- list of servers for mason to install
   ensure_installed = {
-    "sumneko_lua",
+    "lua_ls",
     "clangd",
     "pyright",
     "cmake",
-    "marksman"
+    "marksman",
+    "texlab"
   },
   -- auto-install configured servers (with lspconfig)
   automatic_installation = true, -- not the same as ensure_installed
