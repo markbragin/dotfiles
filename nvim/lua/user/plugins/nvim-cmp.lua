b07b15bed9dd0b446d2f50ccfd9fb9154ed3815a
mark bragin <m4rk.brag1n@gmail.com>
2023-12-28 20:17:01 +0300

    Last sync: 28-12-2023 20:17:01

--- nvim/lua/user/plugins/nvim-cmp.lua
+++ nvim/lua/user/plugins/nvim-cmp.lua
@@ -6,7 +6,11 @@ return {
     'hrsh7th/cmp-path',
     'hrsh7th/cmp-nvim-lsp-signature-help',
     -- snippets
-    'L3MON4D3/LuaSnip',
+    {
+      "L3MON4D3/LuaSnip",
+      version = "v2.*",
+      build = "make install_jsregexp"
+    },
     'saadparwaiz1/cmp_luasnip',
     'rafamadriz/friendly-snippets',
   },
