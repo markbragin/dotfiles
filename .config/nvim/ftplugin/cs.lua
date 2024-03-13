ec0abf812b63906e917b43d61e8c7814c4aa8387
mark bragin <m4rk.brag1n@gmail.com>
2024-03-13 20:51:16 +0300

    lil fix

--- .config/nvim/ftplugin/cs.lua
+++ .config/nvim/ftplugin/cs.lua
@@ -1,7 +1,7 @@
 vim.keymap.set("n", "<F3>", "<cmd>!dotnet build<CR>")
-vim.keymap.set("n", "<leader><F3>", "<cmd>!/usr/local/bin/st -e bash -c 'bin/Debug/net8.0/`basename $PWD`;read'<CR>")
+vim.keymap.set("n", "<leader><F3>", "<cmd>!/usr/local/bin/st -e bash -c 'bin/Debug/net8.0/`basename $PWD`;read -p \"Press any key to continue...\"' <CR>")
 
 vim.keymap.set("n", "<F4>", "<cmd>!dotnet build --configuration Release<CR>")
-vim.keymap.set("n", "<leader><F4>", "<cmd>!/usr/local/bin/st -e bash -c 'bin/Release/net8.0/`basename $PWD`;read'<CR>")
+vim.keymap.set("n", "<leader><F4>", "<cmd>!/usr/local/bin/st -e bash -c 'bin/Release/net8.0/`basename $PWD`;read -p \"Press any key to continue...\"'<CR>")
 
 vim.keymap.set("n", "<leader><F5>", "<cmd>!dotnet run<CR>")
