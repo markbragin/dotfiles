0c9e95c357eed83676a6fcf1ecf68bbd66f8a90e
mark bragin <m4rk.brag1n@gmail.com>
2024-03-18 15:26:16 +0300

    little fix

--- scripts/lock.sh
+++ scripts/lock.sh
@@ -6,15 +6,13 @@ revert() {
 }
 
 suspend_in_background() {
-    (
-        while true; do
-            status=$(xset q | rg "Monitor is (\w+)" -or '$1')
-            if [ $status == "Off" ]; then
-                systemctl suspend
-            fi
-            sleep 5
-        done
-    ) &
+    while true; do
+        status=$(xset q | rg "Monitor is (\w+)" -or '$1')
+        if [ $status == "Off" ]; then
+            systemctl suspend
+        fi
+        sleep 5
+    done
 }
 
 trap revert HUP INT TERM
@@ -22,6 +20,6 @@ trap revert HUP INT TERM
 revert
 xset dpms 30 30 600
 playerctl -a pause
-suspend_in_background
+suspend_in_background &
 i3lock -e -u -n -f -c 000000 -i ~/Pictures/wallpaper.png
 revert
