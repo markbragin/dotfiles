a5baca720165ba813f8fc41aa0044b52821fd5d1
mark bragin <m4rk.brag1n@gmail.com>
2024-03-18 16:46:48 +0300

    added packets for music

--- install.sh
+++ install.sh
@@ -23,7 +23,8 @@ if [[ ${confirm} == [yY] ]]; then
         checkinstall libnotify-bin xinput pulseaudio yad xdotool autoconf \
         pavucontrol network-manager pulseaudio-utils feh atril evince \
         fonts-symbola ristretto ffmpeg thunar arc-theme python3-full \
-        pulseaudio-module-jack acpi playerctl fd-find fzf flameshot
+        pulseaudio-module-jack acpi playerctl fd-find fzf flameshot \
+        cmus youtube-dl eyed3
 fi
 
 # install st
