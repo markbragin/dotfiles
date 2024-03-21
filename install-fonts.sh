17d1962dbac7c440c4ec197a78ef79055a5b7807
mark bragin <m4rk.brag1n@gmail.com>
2024-03-21 17:13:00 +0300

    switched to Nerd font

--- install-fonts.sh
+++ install-fonts.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 cd /tmp
-wget https://download.jetbrains.com/fonts/JetBrainsMono-2.304.zip
-mkdir -p ${HOME}/.fonts/JetBrainsMono
-unzip JetBrainsMono* -d ${HOME}/.fonts/JetBrainsMono
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/JetBrainsMono.zip
+mkdir -p ${HOME}/.fonts/JetBrainsMonoNerd
+unzip JetBrainsMono* -d ${HOME}/.fonts/JetBrainsMonoNerd
 
 wget https://github.com/be5invis/Iosevka/releases/download/v29.0.0/PkgTTC-IosevkaSS04-29.0.0.zip
 mkdir -p ${HOME}/.fonts/IosevkaSS04
