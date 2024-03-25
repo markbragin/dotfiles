8be89dec14d9832a405bc4726e25982305692b84
mark bragin <m4rk.brag1n@gmail.com>
2024-03-25 22:50:25 +0300

    fixed: now laptop locks if lid closed while sound playin

--- scripts/antilocker.sh
+++ scripts/antilocker.sh
@@ -2,8 +2,9 @@
 
 locker_pid=$(ps -eo pid,cmd | rg -i "(\d+) i3lock" -or '$1')
 if [ -z "$locker_pid" ]; then
-    status=$(cat /proc/asound/card*/pcm0p/sub0/status | grep -i running)
-    if [ -z "$status" ]; then
+    sound_running=$(cat /proc/asound/card*/pcm0p/sub0/status | rg -i running)
+    lid_open=$(cat /proc/acpi/button/lid/LID0/state | rg -i open)
+    if [ -z "$sound_running" ] || [ -z "$lid_open" ]; then
         lock
     fi
 fi
