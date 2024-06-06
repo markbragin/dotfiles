e437bf246b701c9fe43123676990c48664c050c9
mark bragin <m4rk.brag1n@gmail.com>
2024-06-06 17:57:05 +0300

    Last sync: 06-06-2024 17:57:05

--- .config/nvim/ftplugin/c.lua
+++ .config/nvim/ftplugin/c.lua
@@ -1,4 +1,6 @@
-vim.keymap.set("n", "<F3>", "<cmd>make -C build/debug<CR>")
-vim.keymap.set("n", "<leader><F3>", "<cmd>make -C build/debug && /usr/local/bin/st -e bash -c 'bin/debug/*;read -p \"Press any key to continue...\"'<CR>")
-vim.keymap.set("n", "<F4>", "<cmd>make -C build/release<CR>")
-vim.keymap.set("n", "<leader><F4>", "<cmd>make -C build/release && /usr/local/bin/st -e bash -c 'bin/release/*;read -p \"Press any key to continue...\"'<CR>")
+if vim.bo.filetype ~= "cpp" then
+  vim.keymap.set("n", "<F3>", "<cmd>make -C build/debug<CR>")
+  vim.keymap.set("n", "<leader><F3>", "<cmd>make -C build/debug && /usr/local/bin/st -e bash -c 'bin/debug/*;read -p \"\nPress any key to continue...\"'<CR>")
+  vim.keymap.set("n", "<F4>", "<cmd>make -C build/release<CR>")
+  vim.keymap.set("n", "<leader><F4>", "<cmd>make -C build/release && /usr/local/bin/st -e bash -c 'bin/release/*;read -p \"\nPress any key to continue...\"'<CR>")
+end
--- .config/nvim/ftplugin/cpp.lua
+++ .config/nvim/ftplugin/cpp.lua
@@ -1,7 +1,7 @@
 vim.keymap.set("n", "<F3>", "<cmd>make -C build/debug<CR>")
-vim.keymap.set("n", "<leader><F3>", "<cmd>make -C build/debug && /usr/local/bin/st -e bash -c 'bin/debug/*;read -p \"Press any key to continue...\"'<CR>")
+vim.keymap.set("n", "<leader><F3>", "<cmd>make -C build/debug && /usr/local/bin/st -e bash -c 'bin/debug/*;echo;read -p \"Press any key to continue...\"'<CR>")
 vim.keymap.set("n", "<F4>", "<cmd>make -C build/release<CR>")
-vim.keymap.set("n", "<leader><F4>", "<cmd>make -C build/release && /usr/local/bin/st -e bash -c 'bin/release/*;read -p \"Press any key to continue...\"'<CR>")
+vim.keymap.set("n", "<leader><F4>", "<cmd>make -C build/release && /usr/local/bin/st -e bash -c 'bin/release/*;echo;read -p \"Press any key to continue...\"'<CR>")
 
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
